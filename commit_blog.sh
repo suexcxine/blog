11a1f006bbaee1e6cebee60f0e28d555ad1943eb
suexcxine <suex.bestwishes@gmail.com>
2016-07-07 12:04:06 +0800

    无论如何都在远程执行git pull

--- commit_blog.sh
+++ commit_blog.sh
@@ -1,2 +1,3 @@
 # 发布博客
-git add source && git commit -m "修改博客" && git push && ssh loc "docker exec hexo sh -c 'git pull'"
+git add source && git commit -m "修改博客" && git push 
+ssh loc "docker exec hexo sh -c 'git pull'"
